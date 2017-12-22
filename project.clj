4b0b86ecee45eab581172b09a106e751c2756c09
Caleb MacDonald Black <calebmacdonaldblack@gmail.com>
2017-12-22 22:32:59 +1000

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-weka "0.1.0-SNAPSHOT"
+(defproject clj-weka "0.1.0"
   :description "Clojure wrapper for the java machine learning library weka"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [nz.ac.waikato.cms.weka/weka-stable "3.8.1"]
